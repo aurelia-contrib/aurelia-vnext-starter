741b90b445a1803ad05accf2ddf8eb581ac975e7
Fred Kleuver <fred@avurad.nl>
2018-09-18 12:15:46 +0200

    hook into parcel's HMR

--- src/startup.ts
+++ src/startup.ts
@@ -2,8 +2,19 @@ import { BasicConfiguration } from '@aurelia/jit';
 import { Aurelia } from '@aurelia/runtime';
 import { App } from './app';
 
-window['au'] = new Aurelia()
+const au = window['au'] = new Aurelia()
   .register(BasicConfiguration)
   .app({ host: document.querySelector('app'), component: new App() })
   .start();
 
+declare var module: any;
+
+if (module['hot']) {
+  module['hot'].dispose(() => {
+    au.stop();
+  });
+
+  module['hot'].accept(() => {
+    au.start();
+  });
+}
